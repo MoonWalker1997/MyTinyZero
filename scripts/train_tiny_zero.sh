481e74b9d85d736a3ec1adff9eb1a97d9fc1be46
Tory Li <tuo90515@temple.edu>
2025-02-26 16:50:25 -0500

    Update train_tiny_zero.sh

--- scripts/train_tiny_zero.sh
+++ scripts/train_tiny_zero.sh
@@ -1,17 +1,17 @@
 python3 -m verl.trainer.main_ppo \
 data.train_files=$DATA_DIR/train.parquet \
 data.val_files=$DATA_DIR/test.parquet \
-data.train_batch_size=8 \
-data.val_batch_size=16 \
+data.train_batch_size=4 \
+data.val_batch_size=4 \
 data.max_prompt_length=256 \
-data.max_response_length=1024 \
+data.max_response_length=512 \
 actor_rollout_ref.model.path=$BASE_MODEL \
 actor_rollout_ref.actor.optim.lr=1e-6 \
-actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-actor_rollout_ref.actor.ppo_micro_batch_size=8 \
+actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+actor_rollout_ref.actor.ppo_micro_batch_size=2 \
 actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
 actor_rollout_ref.rollout.tensor_model_parallel_size=$ROLLOUT_TP_SIZE \
-actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.1 \
 actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
 critic.optim.lr=1e-5 \
 critic.model.path=$BASE_MODEL \
